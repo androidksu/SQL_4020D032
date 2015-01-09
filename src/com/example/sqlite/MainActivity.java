91ec87664c5d3dfe10a62e7fc101be101b482166
yongsiang0210 <yong.siang0210@gmail.com>
2015-01-09 12:22:28 +0800

    init

--- src/com/example/sqlite/MainActivity.java
+++ src/com/example/sqlite/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sqlite;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +14,10 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	EditText name, number;
 	TextView tv;
-
+	
+	SQLiteDatabase db;
+	SQLite dbHelper;
+	String DATABASE_TABLE ="table1";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,25 +34,76 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	// 新增
+	// �啣�
 	public void bt1(View v) {
+		String aa=name.getText().toString();
+		String bb=number.getText().toString();
+		dbHelper = new SQLite(getBaseContext());
+		db = dbHelper.getWritableDatabase();
+		
+		ContentValues cv = new ContentValues();
+		cv.put("Name",aa);
+		cv.put("Number",bb);
+		db.insert(DATABASE_TABLE,null,cv);
+		
+		dbHelper.close();
+		db.close();	
+		tv.setText("新增一筆資料");
 	}
 
-	// 移除
+	// 蝘駁
 	public void bt2(View v) {
-
+		String aa=name.getText().toString();
+		String bb=number.getText().toString();
+		dbHelper = new SQLite(getBaseContext());
+		db = dbHelper.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		cv.put("Number",bb);
+		db.update(DATABASE_TABLE, cv, "Name='"+ aa +"'", null);
+		dbHelper.close();
+		db.close();	
+		tv.setText("修改一筆資料");
+		
 	}
 
-	// 修改
+	// 靽格
 	public void bt3(View v) {
-
+		String aa=name.getText().toString();
+		dbHelper = new SQLite(getBaseContext());
+		db = dbHelper.getWritableDatabase();
+		db.delete(DATABASE_TABLE,"Name='"+ aa +"'", null);
+		dbHelper.close();
+		db.close();	
+		tv.setText("刪除一筆資料");
 	}
 
-	// 查詢
+	// �亥岷
 	public void bt4(View v) {
+String[] item = {"_id","Name","Number"};
+StringBuffer sb = new StringBuffer();
+sb.append("ID");
+sb.append("\t");
+sb.append("姓名");
+sb.append("\t\t\t");
+sb.append("學號");
+sb.append("\n");
+	
+	dbHelper = new SQLite(getBaseContext());
+db = dbHelper.getWritableDatabase();
 
+Cursor c = db.query(DATABASE_TABLE,item,null,null,null,null,null);
+c.moveToFirst();
+for (int i = 0; i<c.getCount();i++){
+	sb.append(c.getString(0));
+	sb.append("\t");
+	sb.append(c.getString(1));
+	sb.append("\t");
+	sb.append(c.getString(2));
+	sb.append("\n");
+	c.moveToNext();
+}
+tv.setText(sb);
 	}
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
